e13ed71148b9e512004c5cb19aea8e18efa180f5
killflex <ferryhasanbackup@gmail.com>
2025-04-17 12:56:16 +0700

    put index.js to root folder and vercel.json destination to root

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-const mainRouter = require("./routes/index.js");
-const middlewareLogRequest = require("./middleware/logs.js");
+const mainRouter = require("./src/routes/index.js");
+const middlewareLogRequest = require("./src/middleware/logs.js");
 
 const app = express();
 
