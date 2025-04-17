710c3bac304fe59d647c8aec9734c63ccfa344dc
killflex <ferryhasanbackup@gmail.com>
2025-04-17 13:06:24 +0700

    fixing vercel.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,6 @@ const router = express.Router();
 
 const usersRoutes = require("./users.js");
 
-router.use("/", (req, res) => {
-  res.send("Laba-laba sunda!");
-});
 router.use("/users", usersRoutes);
 
 module.exports = router;
